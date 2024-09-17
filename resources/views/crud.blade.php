a9deb505263b22c2821bb4c20eec981e13291722
Meekail Hanis <Meekailhanis2006@gmail.com>
2024-09-17 16:30:11 +0530

    html change

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -8,7 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                <h3 class="text-lg font-semibold mb-4">Manage Your Data</h3>
                 
                 <!-- Books Management -->
                 <div class="mb-8">
